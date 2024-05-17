1728d4e5df447006b15badbf4084937fc503abda
Adwoa <baahbrako@gmail.com>
2024-05-17 13:47:52 +0000

    adding header details

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,28 @@ import NavBar from "@/components/menu/NavBar";
 import ResultDetails from "@/components/results/ResultDetails";
 import SearchResults from "@/components/results/SearchResults";
 import SideMenu from "@/components/menu/SideMenu";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <main>
-      <NavBar />
-      <section className="flex">
-        <SideMenu />
-        <section className="flex flex-col">
-          <FilterMenu />
-          <div className="border-b border-b-amaka-grey-border w-full"></div>
-          <section id="resultsArea" className="flex pr-14">
-            <SearchResults />
-            <ResultDetails />
+    <>
+      <Head>
+        <title>Amaka Gigs Web Page</title>
+      </Head>
+      <main>
+        <NavBar />
+        <section className="flex">
+          <SideMenu />
+          <section className="flex flex-col">
+            <FilterMenu />
+            <div className="border-b border-b-amaka-grey-border w-full"></div>
+            <section id="resultsArea" className="flex pr-14">
+              <SearchResults />
+              <ResultDetails />
+            </section>
           </section>
         </section>
-      </section>
-    </main>
+      </main>
+    </>
   );
 }
