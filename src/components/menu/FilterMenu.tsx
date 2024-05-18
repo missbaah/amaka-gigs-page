1c9992457b88a2586609e9d0390f5c42c274cb4c
Adwoa <baahbrako@gmail.com>
2024-05-18 11:43:35 +0000

    style changes

--- src/components/menu/FilterMenu.tsx
+++ src/components/menu/FilterMenu.tsx
@@ -9,7 +9,7 @@ const FilterMenu: FC = () => {
 
   return (
     <>
-      <div className="flex px-4 items-center justify-start gap-3 py-2 w-full">
+      <div className="flex px-4 items-center justify-start gap-3 py-2 w-full lg:flex-nowrap flex-wrap">
         {filters}
         <span className="border h-6"></span>
         <button className="px-4 h-8 flex gap-4 items-center rounded-full text-sm border border-amaka-grey-border">
